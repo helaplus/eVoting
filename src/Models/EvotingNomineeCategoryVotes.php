b3d2f8782f5c5a843ec645dfb7821eb727f57fc2
le-yo <le-yo@leyo.com>
2022-03-29 00:46:27 +0300

    Added votes table

---
+++ src/Models/EvotingNomineeCategoryVotes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Helaplus\Evoting\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EvotingNomineeCategoryVotes extends Model
+{
+    use HasFactory;
+}
