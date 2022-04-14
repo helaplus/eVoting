666d60db0597b0df3cbc25f6a78d5b799a24b41f
le-yo <le-yo@le-yo.com>
2022-04-14 16:57:54 +0300

    Testing views

---
+++ src/Http/Controllers/EvotingCategories.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Helaplus\Evoting\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EvotingCategories extends Controller
+{
+
+
+    public function index(Request $request){
+//        $posts = EvotingCategory::all();
+//        print_r($posts);
+//        exit;
+//        return view('blogpackage::posts.index', compact('posts'));
+        echo "hapa";
+        exit;
+
+    }
+
+
+}
