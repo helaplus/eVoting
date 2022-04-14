4086681275b3fd083c547339242343bcbbb3f930
le-yo <le-yo@le-yo.com>
2022-04-14 16:57:45 +0300

    Updated routing

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Helaplus\Evoting\Http\Controllers\EvotingController;
 //
-Route::post('/home', [EvotingController::class, 'home'])->name('evoting.app');
+//Route::post('/home', [EvotingController::class, 'home'])->name('evoting.app');
+//Route::get('/home', [EvotingController::class, 'home'])->name('evoting.app');
+//
+Route::get('/categories', [EvotingController::class, 'index'])->name('categories.index'); 
